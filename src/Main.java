10a63348f71e8d6224b5f95d1b602e9117be6845
NikitaDr-ON <nikitokdr@gmail.com>
2024-11-19 22:11:57 +0400

    proxy homework

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        EntryPoint entryPoint = new EntryPoint();
+        entryPoint.connectToDataBase();
     }
 }
